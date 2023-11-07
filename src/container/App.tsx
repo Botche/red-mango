d01b19591a7cf46d461e3a9f0be2390f56fb69d4
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-07 15:05:17 +0200

    Fetch Menu Items and Interfaces in TS

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import { Header, Footer } from "../components/layout";
+import { MenuItemModel } from "../interfaces";
 
 function App() {
+  const [menuItems, setMenuItems] = useState<MenuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch("https://redmangoapidotnet.azurewebsites.net/api/MenuItem")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
     <div className="">
       <Header />
---
+++ src/interfaces/menuItemModel.tsx
@@ -0,0 +1,9 @@
+export default interface MenuItemModel {
+  name: string;
+  description: string;
+  specialTag: string;
+  category: string;
+  price: number;
+  imageUrl: string;
+  id: string;
+}
