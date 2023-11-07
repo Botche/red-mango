4b6e05a1786dba5db21462d7e7064227dcd94944
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-07 17:03:45 +0200

    Add Mini Loader Component on Home Page

--- src/components/page/menuItems/MenuItemCard.tsx
+++ src/components/page/menuItems/MenuItemCard.tsx
@@ -59,18 +59,34 @@ function MenuItemCard(props: MenuItemCardProps) {
               </i>
             )}
 
-          <i
-            className="bi bi-cart-plus btn btn-outline-danger"
-            style={{
-              position: "absolute",
-              top: "15px",
-              right: "15px",
-              padding: "5px 10px",
-              borderRadius: "3px",
-              outline: "none !important",
-              cursor: "pointer",
-            }}
-          ></i>
+          {isAddingToCart ? (
+            <div
+              style={{
+                position: "absolute",
+                top: "15px",
+                right: "15px",
+              }}
+            >
+              <div
+                className="spinner-border text-warning"
+                style={{ scale: "100%" }}
+              ></div>
+            </div>
+          ) : (
+            <i
+              className="bi bi-cart-plus btn btn-outline-danger"
+              style={{
+                position: "absolute",
+                top: "15px",
+                right: "15px",
+                padding: "5px 10px",
+                borderRadius: "3px",
+                outline: "none !important",
+                cursor: "pointer",
+              }}
+              onClick={() => handleAddToCart(props.menuItem.id)}
+            ></i>
+          )}
 
           <div className="text-center">
             <p className="card-title m-0 text-success fs-3">
