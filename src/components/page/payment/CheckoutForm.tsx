5cf06324f2b09124d7f02e8783786f21c8aa4f03
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-08 16:30:43 +0200

    Stripe Processing

--- src/components/page/payment/CheckoutForm.tsx
+++ src/components/page/payment/CheckoutForm.tsx
@@ -1,8 +1,43 @@
-import { PaymentElement } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import { toastNotify } from "../../../helpers";
 
 function CheckoutForm() {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const stripe = useStripe();
+  const elements = useElements();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      return;
+    }
+
+    setIsProcessing(true);
+
+    const result = await stripe.confirmPayment({
+      elements,
+      confirmParams: {
+        return_url: "https://example.com/order/123/complete",
+      },
+      redirect: "if_required",
+    });
+
+    if (result.error) {
+      toastNotify("An unexpected error occured.", "error");
+      setIsProcessing(false);
+    } else {
+      console.log(result);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <PaymentElement />
       <button className="btn btn-success mt-5 w-100">Submit</button>
     </form>
