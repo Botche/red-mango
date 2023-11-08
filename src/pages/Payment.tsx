e1f7e5737102b71fadde7df9e49ba669a0f2442a
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-08 13:59:54 +0200

    Order Summary UI

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -2,22 +2,32 @@ import { useLocation } from "react-router-dom";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import { CheckoutForm } from "../components/page/payment";
+import { OrderSummary } from "../components/page/order";
 
-console.log(process.env)
+console.log(process.env);
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLISH_KEY!);
 
 function Payment() {
   const {
     state: { apiResult, userInput },
   } = useLocation();
-  
+
   const options = {
     clientSecret: apiResult.clientSecret,
   };
 
   return (
     <Elements stripe={stripePromise} options={options}>
-      <CheckoutForm />
+      <div className="d-flex justify-content-center">
+        <div className="container m-5 p-5 row">
+          <div className="col-md-7">
+            <OrderSummary />
+          </div>
+          <div className="col-md-5">
+            <CheckoutForm />
+          </div>
+        </div>
+      </div>
     </Elements>
   );
 }
