61a99c44f9583755a9733821dec2ebd89c7846a6
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-09 15:54:52 +0200

    Menu Item API Mutations

--- src/apis/menuItemApi.ts
+++ src/apis/menuItemApi.ts
@@ -19,8 +19,43 @@ const menuItemApi = createApi({
       }),
       providesTags: ["MenuItems"],
     }),
+    createMenuItem: builder.mutation({
+      query: (data) => ({
+        url: `menuItem`,
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: data,
+      }),
+      invalidatesTags: ["MenuItems"],
+    }),
+    updateMenuItem: builder.mutation({
+      query: ({ data, id }) => ({
+        url: `menuItem/${id}`,
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: data,
+      }),
+      invalidatesTags: ["MenuItems"],
+    }),
+    deleteMenuItem: builder.mutation({
+      query: (id) => ({
+        url: `menuItem/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["MenuItems"],
+    }),
   }),
 });
 
-export const { useGetMenuItemByIdQuery, useGetMenuItemsQuery } = menuItemApi;
+export const {
+  useGetMenuItemByIdQuery,
+  useGetMenuItemsQuery,
+  useCreateMenuItemMutation,
+  useUpdateMenuItemMutation,
+  useDeleteMenuItemMutation,
+} = menuItemApi;
 export default menuItemApi;
