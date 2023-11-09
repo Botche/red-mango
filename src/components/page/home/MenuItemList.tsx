a840f4bb44e0f3d9b322bc71a3e56ed70c2ea183
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-09 17:38:58 +0200

    Implement Search Functionality

--- src/components/page/home/MenuItemList.tsx
+++ src/components/page/home/MenuItemList.tsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { MenuItemModel } from "../../../interfaces";
 import MenuItemCard from "./MenuItemCard";
 import { RootState } from "../../../storage/redux/store";
 
 function MenuItemList() {
-  const menuItems = useSelector(
-    (state: RootState) => state.menuItemStore.menuItem
+  const { search: searchValue, menuItem: data } = useSelector(
+    (state: RootState) => state.menuItemStore
   );
+  const [menuItems, setMenuItems] = useState<MenuItemModel[]>(data);
+
+  useEffect(() => {
+    if (data) {
+      const tempMenuArray = handleFilters(searchValue);
+      setMenuItems(tempMenuArray);
+    }
+  }, [searchValue, data]);
+
+  const handleFilters = (search: string) => {
+    let tempMenuItems = [...data];
+
+    if (search) {
+      const tempSearchMenuItems = [...tempMenuItems];
+      tempMenuItems = tempSearchMenuItems.filter((item: MenuItemModel) =>
+        item.name.toUpperCase().includes(search.toUpperCase())
+      );
+    }
+
+    return tempMenuItems;
+  };
 
   return (
     <div className="container row">
