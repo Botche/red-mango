49e5dcd27b418f27200368f7c04c01eb11709132
Gabriel Petkov <gabrielpetkov622@gmail.com>
2023-11-10 20:16:33 +0200

    Update MenuItemList.tsx

--- src/pages/menuItem/MenuItemList.tsx
+++ src/pages/menuItem/MenuItemList.tsx
@@ -30,7 +30,7 @@ function MenuItemList() {
   return (
     <div className="table p-5">
       <div className="d-flex align-items-center justify-content-between">
-        <h1 className="text-success">MenuItem List</h1>
+        <h1 className="text-success">Menu Items List</h1>
         <button
           className="btn btn-success"
           onClick={() => navigate(ROUTES.createMenuItem)}
